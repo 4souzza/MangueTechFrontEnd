06ebd8fbc5242630a7df19fa64b0493e3b53d436
Aldair Soares <aldair.soares.as@gmail.com>
2021-12-09 15:28:07 -0300

    @aldairsoares [CREATED: Login, model and service]

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor( 
+    private http: HttpClient
+  ) { }
+  entrar(usuarioLogin: UsuarioLogin):Observable<UsuarioLogin>{
+    return this.http.post<UsuarioLogin>('https://manguetech.herokuapp.com/usuarios/logar',usuarioLogin)
+  }
+
+  cadastrar(usuario: Usuario):Observable<Usuario>{
+    return this.http.post<Usuario>('https://manguetech.herokuapp.com/usuarios/cadastrar',usuario)
+
+  }
+}
